670d262e8c918a7f47ceb4bd96b2d6f6405f330b
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2024-10-01 16:59:20 -0400

    Add files via upload

---
+++ master/master.bat
@@ -0,0 +1,52 @@
+@echo off
+Setlocal EnableDelayedExpansion
+
+FOR /F "tokens=1,2 delims==" %%G IN (master.properties) DO (
+    set %%G=%%H
+)
+
+set dir=%cd%
+
+set "workspacePath2=%workspacePath%"
+set "workspacePath2=!workspacePath2:\=\\!"
+if not exist "%workspacePath%" mkdir "%workspacePath%"
+
+start "Settings.bat" /d ".\scripts" /MIN "Settings.bat"
+start "VS_Install.bat" /d ".\scripts" "VSInstall.bat"
+start "Download_Software.bat" /d ".\scripts" "Download_Software.bat"
+start "clone.bat" /d ".\scripts" /MIN "clone.bat"
+call .\scripts\classpath.bat
+
+pause
+
+call .\scripts\CheckRDF_Map.bat
+call .\scripts\socialProperties_Update.bat
+call .\scripts\server.bat
+call .\scripts\createTomcat.bat
+
+pause
+
+call .\scripts\WebXml.bat
+if %setEnvVariables%==true (
+    call .\scripts\Environment_Variables.bat
+)
+
+pause
+
+call .\scripts\maven.bat
+call .\scripts\catalina.bat
+call .\scripts\maven.bat
+
+pause
+
+if %rInstall%==true (
+    call .\scripts\R_Install.bat
+)
+@REM call anaconda.bat
+
+pause
+
+call .\scripts\pythonlibs.bat
+call .\scripts\pnpm.bat
+call .\scripts\semoss_Update.bat
+call .\scripts\launch.bat
