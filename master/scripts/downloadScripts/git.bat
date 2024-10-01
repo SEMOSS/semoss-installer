b7687351e4ebb695068a478fa48cbb944c504c11
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2024-10-01 17:00:20 -0400

    Add files via upload

---
+++ master/scripts/downloadScripts/git.bat
@@ -0,0 +1,5 @@
+@echo off
+
+curl -o %UserProfile%\Downloads\Git-2.46.1-64-bit.exe %Git_Download%
+
+exit
---
+++ master/scripts/downloadScripts/notepad.bat
@@ -0,0 +1,5 @@
+@echo off
+
+curl -o %UserProfile%\Downloads\npp.7.8.2.Installer.x64.exe %Notepad_Download%
+
+exit
