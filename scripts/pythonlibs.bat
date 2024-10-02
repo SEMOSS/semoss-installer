35a1a5501e121be6a2d208e050c3429c6d1df20d
Gyllian Zuniga <93683626+ggaylor1@users.noreply.github.com>
2024-10-02 13:41:15 -0700

    Update pythonlibs.bat
    took away -g

--- scripts/pythonlibs.bat
+++ scripts/pythonlibs.bat
@@ -3,12 +3,14 @@
 @REM Set up the Developer Command Prompt environment for 64-bit
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
+::write some code that checks if python is installed?
+
 @REM Install Python packages
 echo Installing Python packages
-call pip install -g pandas
-call pip install -g fuzzywuzzy
-call pip install -g python-Levenshtein
-call pip install -g --upgrade -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/R4.2.1-debian11/semoss_requirements.txt
-call pip install -g --upgrade -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/cuda12-R4.2.1/cfgai_requirements.txt
+call pip install pandas
+call pip install fuzzywuzzy
+call pip install python-Levenshtein
+call pip install --upgrade -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/R4.2.1-debian11/semoss_requirements.txt
+call pip install --upgrade -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/cuda12-R4.2.1/cfgai_requirements.txt
 
 @REM Exit the Developer Command Prompt
