af72213b3d39bffa10eadd92748123ec41026a11
adorsey8462 <140740499+adorsey8462@users.noreply.github.com>
2024-10-03 13:26:26 -0400

    Update maven.bat

--- scripts/maven.bat
+++ scripts/maven.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start "Semoss Clean & Install" cmd.exe /c "cd %workspacePath%\Semoss & mvn clean install -U -DskipTests=true"
-start /wait /MIN "Monolith Clean & Install" cmd.exe /c "cd %workspacePath%\Monolith & mvn clean install -U -DskipTests=true"
+start /wait "Semoss Clean & Install" cmd.exe /c "cd %workspacePath%\Semoss & mvn clean install -U -DskipTests=true"
+start /wait "Monolith Clean & Install" cmd.exe /c "cd %workspacePath%\Monolith & mvn clean install -U -DskipTests=true"
