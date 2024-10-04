c21394aac7c3c8f4dbf8420a9ec06ed764322ba6
adorsey8462 <140740499+adorsey8462@users.noreply.github.com>
2024-10-04 14:20:01 -0400

    Update master.bat
    Added call to importSettings.bat

--- master.bat
+++ master.bat
@@ -28,6 +28,7 @@ start "clone.bat" /d ".\scripts" /B /W "clone.bat" >> logs/clone.log 2>&1
 
 call .\scripts\classpath.bat
 call .\scripts\classpathSemoss.bat
+call .\scripts\importSettings.bat
 call .\scripts\CheckRDF_Map.bat
 call .\scripts\socialProperties_Update.bat
 call .\scripts\server.bat
