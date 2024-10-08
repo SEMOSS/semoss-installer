8d4fbd118dde1f65fd56fc3665a9f3e57f3f7e30
shrayDe <shrswarup@deloitte.com>
2024-10-08 14:08:25 -0700

    Tomcat Version Change

--- scripts/downloadScripts/tomcat.bat
+++ scripts/downloadScripts/tomcat.bat
@@ -1,12 +1,15 @@
 @echo off
 
-curl -o %UserProfile%\Downloads\apache-tomcat-9.0.95-windows-x64.zip %Tomcat_Download%
-tar -zxvf %UserProfile%\Downloads\apache-tomcat-9.0.95-windows-x64.zip --directory "%workspacePath%"
+Tomcat_Version=apache-tomcat-9.0.96
+
+curl -o %UserProfile%\Downloads\%Tomcat_Version%-windows-x64.zip %Tomcat_Download%
+tar -zxvf %UserProfile%\Downloads\%Tomcat_Version%-windows-x64.zip --directory "%workspacePath%"
 
 :: Ensures a apache-tomcat folder exist
 if not exist "%workspacePath%\%Tomcat_Version%" (
     :: DO SOMETHING HERE
     echo Apache-tomcat folder does not exist at %workspacePath%\%Tomcat_Version%
+    echo Please check versions and try again.
     pause
 )
 
