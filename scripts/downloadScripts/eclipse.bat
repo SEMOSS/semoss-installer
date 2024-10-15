c7d320f015dbe2ea8df66bace81ca59f8a7be634
AndrewRodddd <62724891+AndrewRodddd@users.noreply.github.com>
2024-10-15 11:06:34 -0400

    UPDATE: Corrected eclipse unzip location

--- scripts/downloadScripts/eclipse.bat
+++ scripts/downloadScripts/eclipse.bat
@@ -1,6 +1,6 @@
 @echo off
 
 curl -o %UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip %Eclipse_Download%
-tar -zxvf %UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip --directory %UserProfile%\Downloads
+tar -zxvf %UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip --directory %UserProfile%\Desktop
 
 exit
