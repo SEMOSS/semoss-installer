c55dfad72730eb091a7eec14bb1a878a0f4476dd
shrayDe <shrswarup@deloitte.com>
2024-10-16 13:20:26 -0700

    Remove Concurrency / Update Commands

--- scripts/pythonlibs.bat
+++ scripts/pythonlibs.bat
@@ -1,8 +1,8 @@
 @echo off
 
-start cmd /c "cd %UserProfile% & pipx install virtualenv"
+start /wait cmd /c "cd %UserProfile% & pipx install virtualenv"
 
-start cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install pandas & pip install fuzzywuzzy & pip install python-Levenshtein & pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-cpu/pinned-requirements.txt"
+start /wait cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-cpu/pinned-requirements.txt"
 
 ::if your PC has a GPU, replace the last command with the one below:
 ::pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-gpu/pinned-requirements.txt"
