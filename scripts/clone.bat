92b18981809e65544025ba9d163ebe774ba8099a
shrayDe <shrswarup@deloitte.com>
2024-10-16 14:16:42 -0700

    Add concurrency for python and R

--- scripts/clone.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-Setlocal EnableDelayedExpansion
-
-start "Monolith Clone" /d "%workspacePath%" /MIN "%dir%\scripts\cloneScripts\monoClone.bat"
-start "SemossWeb Clone" /d "%workspacePath%\%Tomcat_Version%\webapps" /MIN "%dir%\scripts\cloneScripts\semossWebClone.bat"
-start "Semoss Clone" /d "%workspacePath%" /MIN /W "%dir%\scripts\cloneScripts\semossClone.bat"
