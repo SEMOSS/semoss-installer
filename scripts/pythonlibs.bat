13be345a3feb816c67c302c393feeee728a776e9
Gyllian Zuniga <93683626+ggaylor1@users.noreply.github.com>
2024-10-16 11:25:51 -0700

    Update pythonlibs.bat
    changed to virtual environment

--- scripts/pythonlibs.bat
+++ scripts/pythonlibs.bat
@@ -1,16 +1,10 @@
 @echo off
 
-@REM Set up the Developer Command Prompt environment for 64-bit
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+start cmd /c "cd %UserProfile% & pipx install virtualenv"
 
-::write some code that checks if python is installed?
+start cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install pandas & pip install fuzzywuzzy & pip install python-Levenshtein & pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-cpu/pinned-requirements.txt"
 
-@REM Install Python packages
-echo Installing Python packages
-call pip install pandas
-call pip install fuzzywuzzy
-call pip install python-Levenshtein
-call pip install --upgrade -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/R4.2.1-debian11/semoss_requirements.txt
-call pip install --upgrade -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/cuda12-R4.2.1/cfgai_requirements.txt
+::if your PC has a GPU, replace the last command with the one below:
+::pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-gpu/pinned-requirements.txt"
 
-@REM Exit the Developer Command Prompt
+@REM Exit 
