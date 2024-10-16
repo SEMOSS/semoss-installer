056555304e624573907558fe306847ead86f545b
Marielle Prihoda <90920625+marprihoda23@users.noreply.github.com>
2024-10-16 14:59:17 -0400

    Update eclipse.bat

--- scripts/downloadScripts/eclipse.bat
+++ scripts/downloadScripts/eclipse.bat
@@ -1,6 +1,17 @@
 @echo off
+setlocal
+set "zipFilePath=%UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip"
+set "destinationFolder=%UserProfile%\Downloads"
 
-curl -o %UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip %Eclipse_Download%
-tar -zxvf %UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip --directory %UserProfile%\Desktop
+curl -L -o "%zipFilePath%" "%Eclipse_Download%"
+echo Unzipping Eclipse ...
+tar -xf "%zipFilePath%" -C "%UserProfile%\Desktop"
 
+if %errorlevel% neq 0 (
+    echo Error: Failed to unzip the file.
+    exit /b 1
+)
+echo Eclipse has ben unZipped to Desktop!
+
+endlocal
 exit
