80775804124e260c74fcdbd39ad423a62937bdd1
Marielle Prihoda <90920625+marprihoda23@users.noreply.github.com>
2024-10-17 10:07:29 -0400

    Update Download_Software.bat

--- scripts/Download_Software.bat
+++ scripts/Download_Software.bat
@@ -8,7 +8,7 @@ start "Tomcat Download" /MIN ".\downloadScripts\tomcat.bat"
 if %softawareInstall%==true (
 
     if %eclipseFlag%==true (
-        start "Eclipse Download" /MIN ".\downloadScripts\eclipse.bat"
+        call .\downloadScripts\eclipse.bat
         set "eclipseLoc=%UserProfile%\Desktop\eclipse"
     )
 
