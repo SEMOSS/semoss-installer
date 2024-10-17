52063408daa5677dfcf4438e57d80eab96ae9870
shrayDe <shrswarup@deloitte.com>
2024-10-17 11:58:19 -0700

    Library Download in same window

--- scripts/pythonlibs.bat
+++ scripts/pythonlibs.bat
@@ -2,11 +2,22 @@
 
 start /wait cmd /c "cd %UserProfile% & pipx install virtualenv"
 
+@REM if %haveGPU%==true (
+@REM     start /wait cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-gpu/pinned-requirements.txt"
+@REM ) else (
+@REM     start /wait cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-cpu/pinned-requirements.txt"
+@REM )
+
 if %haveGPU%==true (
-    start /wait cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-gpu/pinned-requirements.txt"
+    cd %workspacePath%\Semoss
+    virtualenv venv
+    call venv\Scripts\activate
+    call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-gpu/pinned-requirements.txt
 ) else (
-    echo false
-    start /wait cmd /c "cd %workspacePath%\Semoss & virtualenv venv & venv\Scripts\activate & call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-cpu/pinned-requirements.txt"
+    cd %workspacePath%\Semoss
+    virtualenv venv
+    call venv\Scripts\activate
+    call pip install -r https://raw.githubusercontent.com/SEMOSS/docker-r-python/dev-setup/dev-cpu/pinned-requirements.txt
 )
 
 exit 
