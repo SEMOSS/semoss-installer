81d47e3ba39ab4069866263895384505fc436d73
shrayDe <shrswarup@deloitte.com>
2024-10-17 08:51:43 -0700

    Flag Updates

--- scripts/VSInstall.bat
+++ scripts/VSInstall.bat
@@ -2,15 +2,17 @@
 
 :: Check if VS 2019 is already installed, then make sure they have all options installed
 if not exist %VS_Path% (
-    echo Downloading Visual Studio 2019
+    if vsFlag==true (
+        echo Downloading Visual Studio 2019
+        curl -o %UserProfile%\Downloads\vs_BuildTools.exe %VS_Download%
+        start %UserProfile%\Downloads\vs_BuildTools.exe
+        echo Check the "Desktop development with C++" box. 
+        echo On the right side there will be an "Installation details" section. Make sure to select each checkbox under the Optional section. Download and install the selection
+    ) else (
+        echo Visual Studio 2019 not found. Please change vsFlag to true in properties and try again.
+        pause
+    )
 
-    curl -o %UserProfile%\Downloads\vs_BuildTools.exe %VS_Download%
-    start %UserProfile%\Downloads\vs_BuildTools.exe
-
-    echo .
-    echo Check the "Desktop development with C++" box. 
-    echo On the right side there will be an "Installation details" section. Make sure to select each checkbox under the Optional section. Download and install the selection
-    
 ) else (
     echo Visual Studio 2019 is already installed.
     echo .
