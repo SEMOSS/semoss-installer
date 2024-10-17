79ee9a0db09ae9e06434049c0357fe8c79e21178
Marielle Prihoda <90920625+marprihoda23@users.noreply.github.com>
2024-10-17 10:06:45 -0400

    Update eclipse.bat

--- scripts/downloadScripts/eclipse.bat
+++ scripts/downloadScripts/eclipse.bat
@@ -2,8 +2,14 @@
 setlocal
 set "zipFilePath=%UserProfile%\Downloads\eclipse-jee-2019-09-R-win32-x86_64.zip"
 set "destinationFolder=%UserProfile%\Downloads"
+set Eclipse_Download="https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2019-09/R/eclipse-jee-2019-09-R-win32-x86_64.zip"
 
-curl -L -o "%zipFilePath%" "%Eclipse_Download%"
+
+start /wait curl -L -o "%zipFilePath%" "%Eclipse_Download%"
+if %errorlevel% neq 0 (
+    echo Error: Failed to download the file.
+    exit /b 1
+)
 echo Unzipping Eclipse ...
 tar -xf "%zipFilePath%" -C "%UserProfile%\Desktop"
 
