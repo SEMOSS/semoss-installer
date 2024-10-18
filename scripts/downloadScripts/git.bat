3a52bba1e2c5c129c9b1faa79cdf986e3f62d1a4
shrayDe <shrswarup@deloitte.com>
2024-10-18 13:16:16 -0700

    Update git.bat

--- scripts/downloadScripts/git.bat
+++ scripts/downloadScripts/git.bat
@@ -1,9 +1,7 @@
 @echo off
 Setlocal EnableDelayedExpansion
 
-echo -- Installing Git...
 set "destinationPath=%UserProfile%\Downloads\Git-2.46.1-64-bit.exe"
-echo !destinationPath!
 curl -L -o "!destinationPath!" %Git_Download%
 "!destinationPath!" /VERYSILENT /NORESTART
 
