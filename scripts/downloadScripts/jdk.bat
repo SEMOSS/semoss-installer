dcfa37c3b8215e7c1870fd5cba5b8486e97e517d
Marielle Prihoda <90920625+marprihoda23@users.noreply.github.com>
2024-10-18 16:42:43 -0400

    Update timeout length jdk.bat

--- scripts/downloadScripts/jdk.bat
+++ scripts/downloadScripts/jdk.bat
@@ -4,7 +4,7 @@ set "zipPath=%UserProfile%\Downloads\zulu8.82.0.21-ca-jdk8.0.432-win_x64.zip"
 set "JavaFolder=C:\Program Files\Java"
 
 start /Wait https://cdn.azul.com/zulu/bin/zulu8.82.0.21-ca-jdk8.0.432-win_x64.zip
-timeout /t 60 /nobreak
+timeout /t 90 /nobreak
 
 
 if not exist "%javaFolder%" (
