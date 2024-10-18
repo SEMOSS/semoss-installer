f5f27c83ba487229a97be2054c76924584d5b307
shrayDe <shrswarup@deloitte.com>
2024-10-18 12:33:43 -0700

    Minimize Maven Cleaning

--- scripts/maven.bat
+++ scripts/maven.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start /wait "Semoss Clean & Install" cmd.exe /c "cd %workspacePath%\Semoss & mvn clean install -U -DskipTests=true"
-start /wait "Monolith Clean & Install" cmd.exe /c "cd %workspacePath%\Monolith & mvn clean install -U -DskipTests=true"
+start /wait /MIN "Semoss Clean & Install" cmd.exe /c "cd %workspacePath%\Semoss & mvn clean install -U -DskipTests=true"
+start /wait /MIN "Monolith Clean & Install" cmd.exe /c "cd %workspacePath%\Monolith & mvn clean install -U -DskipTests=true"
